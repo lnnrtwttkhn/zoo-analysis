d074566bc6bddb9c0df3ba9ba7f0a60e62417569
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-07-02 14:21:00 +0200

    Add utilities functions to prepare data

---
+++ code/utilities/data.R
@@ -0,0 +1,159 @@
+prepare_data_behavior <- function(cfg, paths) {
+  dt_input <- load_data(paths$input_behavior)
+  # dt_demographics <- load_data(paths$source$demographics) %>%
+  #   .[!(id %in% cfg$sub_exclude), ]
+  dt_output <- dt_input %>%
+    .[, index := NULL] %>%
+    .[, event_type := factor(as.factor(event_type), levels = cfg$event_levels)] %>%
+    .[, onset := as.numeric(onset)] %>%
+    .[, duration := as.numeric(duration)] %>%
+    .[, duration_true := as.numeric(duration_true)] %>%
+    .[, duration_true := NULL] %>%
+    .[, duration_diff := as.numeric(duration_diff)] %>%
+    .[, duration_diff := NULL] %>%
+    .[, accuracy := as.numeric(accuracy)] %>%
+    assertr::assert(in_set(0, 1), accuracy) %>%
+    .[, stim_file := as.factor(stim_file)] %>%
+    .[, stim_name := as.factor(stim_name)] %>%
+    .[, stim_name := NULL] %>%
+    .[, event_limit := as.numeric(event_limit)] %>%
+    .[, id := as.factor(id)] %>%
+    .[, study_type := factor(as.factor(study_type), levels = c("MRI"))] %>%
+    .[, study_type := NULL] %>%
+    .[, study_name := factor(as.factor(study_name), levels = c("zoo"))] %>%
+    .[, study_name := NULL] %>%
+    .[, session := factor(as.factor(session), levels = c("ses-01", "ses-02"))] %>%
+    .[, condition := dplyr::case_when(
+      condition == "training" ~ "Training",
+      condition == "recall" ~ "Single",
+      condition == "main" ~ "Sequence"
+    )] %>%
+    .[, condition := factor(as.factor(condition), levels = cfg$condition_levels)] %>%
+    .[, run := as.factor(run)] %>%
+    .[, graph := as.factor(graph)] %>%
+    verify(!is.na(graph)) %>%
+    .[, graph_condition := as.factor(graph_condition)] %>%
+    verify(!is.na(graph_condition)) %>%
+    .[, graph_condition := NULL, ] %>%
+    .[, node := factor(as.factor(node), levels = cfg$nodes_letters)] %>%
+    .[, trial_run := as.numeric(trial_run)] %>%
+    .[, num_trials_run := as.numeric(num_trials_run)] %>%
+    .[, num_trials_run := NULL] %>%
+    .[, key_correct := factor(as.factor(as.character(key_correct)), levels = cfg$key_levels)] %>%
+    .[, hand_correct := factor(as.factor(as.character(hand_correct)), levels = cfg$hand_levels)] %>%
+    .[, finger_correct := factor(as.factor(as.character(finger_correct)), levels = cfg$finger_levels)] %>%
+    .[, trial_accuracy := as.numeric(trial_accuracy)] %>%
+    assertr::assert(in_set(0, 1), trial_accuracy) %>%
+    .[, psychopy_version := NULL] %>%
+    .[, key_pressed := factor(as.factor(as.character(key_pressed)), levels = cfg$key_levels)] %>%
+    .[, hand_pressed := ifelse(is.na(hand_pressed), "n/a", hand_pressed)] %>%
+    .[, hand_pressed := factor(as.factor(as.character(hand_pressed)), levels = cfg$hand_levels)] %>%
+    .[, finger_pressed := ifelse(is.na(finger_pressed), "n/a", finger_pressed)] %>%
+    .[, finger_pressed := factor(as.factor(as.character(finger_pressed)), levels = cfg$finger_levels)] %>%
+    .[, response_time := ifelse(response_time == "n/a", NA, response_time)] %>%
+    .[, response_time := as.numeric(response_time)] %>%
+    # check if response times are below the event / response time limit:
+    verify(response_time[!is.na(response_time) & event_type == "response"] < 
+             event_limit[!is.na(response_time) & event_type == "response"]) %>%
+    verify(response_time[!is.na(response_time) & event_type == "response"] > 0) %>%
+    .[, event_limit := NULL] %>%
+    # check if accuracy if correctly specified (key_pressed == key_correct):
+    verify(as.numeric(key_correct[!is.na(key_pressed) & event_type == "response"] == key_pressed[
+      !is.na(key_pressed) & event_type == "response"]) == accuracy[!is.na(key_pressed) & event_type == "response"]) %>%
+    # add additional variables:
+    .[, key_hand_correct := as.factor(paste0(key_correct, "\n(", hand_correct, ")"))] %>%
+    .[, key_hand_pressed := as.factor(paste0(key_pressed, "\n(", hand_pressed, ")"))] %>%
+    .[, log_response_time := log(response_time)] %>%
+    .[, press_correct := paste(key_correct, paste0("(", hand_correct), finger_correct, "finger)")] %>%
+    # add the previous node:
+    plyr::join(
+      x = .,
+      y = .[event_type == "response"] %>% .[, by = .(id, session, run, condition), node_previous := lag(node)],
+      by = colnames(.)[!stringr::str_detect(colnames(.), "dist|prob")]
+    ) %>%
+    .[, by = .(id, session, run, condition, trial_run), node_previous := node_previous[event_type == "response"]] %>%
+    # add the next node:
+    plyr::join(
+      x = .,
+      y = .[event_type == "response"] %>% .[, by = .(id, session, run, condition), node_next := dplyr::lead(node)],
+      by = colnames(.)[!stringr::str_detect(colnames(.), "dist|prob")]
+    ) %>%
+    .[, by = .(id, session, run, condition, trial_run), node_next := node_next[event_type == "response"]] %>%
+    plyr::join(., graphs, by = c("node_previous", "node")) %>%
+    .[, graph := factor(as.factor(graph), levels = cfg$graph_levels)] %>%
+    setDT(.) %>%
+    .[, run_cond := paste0(run, " (", graph, ")")] %>%
+    .[, dist_dir_label := dplyr::case_when(
+      dist_uni == 1 ~ "uni: 1\n bi: 1",
+      dist_uni == 2 ~ "     2\n     2",
+      dist_uni == 3 ~ "     3\n     3",
+      dist_uni == 4 ~ "     4\n    -2",
+      dist_uni == 5 ~ "     5\n    -1"
+    )] %>%
+    .[, dist_dir_label := factor(as.factor(dist_dir_label), levels = c(
+      "uni: 1\n bi: 1", "     2\n     2", "     3\n     3", "     4\n    -2", "     5\n    -1"))] %>%
+    .[, by = .(id), order := factor(
+      as.factor(ifelse(any(run_cond == "run-01 (uni)"), "uni - bi", "bi - uni")),
+      levels = c("uni - bi", "bi - uni"))] %>%
+    .[, dist_current := dplyr::case_when(
+      graph == "flat" ~ dist_flat,
+      graph == "uni" ~ dist_uni,
+      graph == "bi" ~ dist_bi
+    )] %>%
+    .[, prob_current := dplyr::case_when(
+      graph == "flat" ~ prob_flat,
+      graph == "uni" ~ prob_uni,
+      graph == "bi" ~ prob_bi
+    )] %>%
+    .[, onestep := dplyr::case_when(
+      prob_uni == 0.7 & graph == "uni" ~ "High\n(0.7)",
+      prob_uni == 0.1 & graph == "uni" ~ "Low\n(0.1)",
+      prob_bi == 0.35 & graph == "bi" ~ "High\n(0.35)",
+      prob_bi == 0.1 & graph == "bi" ~ "Low\n(0.1)"
+    )] %>%
+    .[, onestep := factor(as.factor(onestep), levels = c("Low\n(0.1)", "High\n(0.35)", "High\n(0.7)"))] %>%
+    # merge.data.table(x = ., y = dt_demographics, by = c("id", "order")) %>%
+    setcolorder(., c(
+      "id", "session", "condition", "run", "trial_run", "event_type",
+      "node", "node_previous", "node_next",
+      "graph", "accuracy", "response_time", "trial_accuracy",
+      "key_correct", "finger_correct", "hand_correct",
+      "key_pressed", "finger_pressed", "hand_pressed"
+    )) %>%
+    setorder(., id, session, condition, run, trial_run) %>%
+    save_data(., paths$behav_task)
+}
+
+prepare_data_mri_rest <- function(cfg, paths) {
+  dt_input <- load_data(paths$input_mri_rest)
+  dt_output <- dt_input %>%
+    .[classification == "ensemble", ] %>%
+    .[class != "other", ] %>%
+    setnames(old = "sub", new = "id") %>%
+    setnames(old = "ses", new = "session") %>%
+    .[, node := NULL] %>%
+    setnames(old = "class", new = "node") %>%
+    setnames(old = "onset_interval", new = "interval_tr") %>%
+    .[, by = .(id, classification, mask_test, train_set, session, run, node), ":="(
+      num_trs = length(unique(interval_tr)),
+      probability_norm = probability / sum(probability)
+    )] %>%
+    verify(num_trs %in% cfg$rest$num_trs) %>%
+    .[, num_trs := NULL] %>%
+    verify(.[, by = .(id, classification, mask_test, train_set, session, run, trial_run, interval_tr), .(
+      num_classes = .N
+    )]$num_classes == cfg$num_nodes) %>%
+    .[, train_set := ifelse(stringr::str_detect(train_set, "response"), "response", "stimulus")] %>%
+    .[, mask_test := dplyr::case_when(
+      mask_test == "hpc" ~ "hippocampus",
+      mask_test == "vis" ~ "occipito-temporal",
+      mask_test == "mot" ~ "motor",
+      mask_test == "mtl" ~ "medial-temporal"
+    )] %>%
+    group_split(id, classification, train_set, mask_test, session, run) %>%
+    map_df(~.x %>% group_by(trial_run) %>% mutate(trial_index = cur_group_id())) %>%
+    setDT(.) %>%
+    .[, node_previous := NA] %>%
+    setorder(id, classification, train_set, mask_test, session, run, trial_index, interval_tr) %>%
+    save_data(., paths$decoding_rest)
+}
---
+++ code/utilities/utilities.R
@@ -0,0 +1,124 @@
+find_root <- function() {
+  if ( basename(here::here()) == "zoo" ) {
+    path_root <- here::here("zoo-analysis")
+  } else {
+    path_root <- here::here()
+  }
+  return(path_root)
+}
+
+load_packages <- function() {
+  library("here")
+  library("data.table")
+  library("magrittr")
+  library("assertr")
+  library("tidyr")
+  library("dplyr")
+  library("purrr")
+}
+
+load_config <- function() {
+  cfg <- list()
+  cfg$tr <- 1.25
+  cfg$num_nodes <- 6
+  cfg$num_graphs <- 2
+  cfg$nodes_letters <- LETTERS[1:cfg$num_nodes]
+  cfg$lcctrl <- lme4::lmerControl(
+    optimizer = c('bobyqa'),
+    optCtrl = list(maxfun = 100000),
+    calc.derivs = FALSE
+  )
+  cfg$event_levels <- c("fixation", "stimulus", "sri", "response", "feedback", "iti")
+  cfg$key_levels <- c("w", "n", "d", "z", "g", "r", "n/a")
+  cfg$finger_levels <- c("index", "middle", "ring", "n/a")
+  cfg$hand_levels <- c("left", "right", "n/a")
+  cfg$condition_levels <- c("Training", "Single", "Sequence")
+  cfg$graph_levels <- c("uni", "bi", "flat")
+  # configuration parameters for resting-state decoding data:
+  cfg$rest$num_trs <- c(233, 137)
+  return(cfg)
+}
+
+create_paths <- function() {
+  path_root <- find_root()
+  datetime <- strftime(Sys.time(), format = "%y-%m-%d_%H-%M-%S")
+  paths <- list()
+  paths$code <- file.path(path_root, "code")
+  paths$input <- file.path(path_root, "input")
+  paths$output <- file.path(path_root, "output")
+  paths$input_behavior <- file.path(paths$input, "bids", "*", "*", "func", "*events")
+  paths$input_mri_rest <- file.path(paths$input, "decoding", "sub-*", "decoding", "*scheme-7*_time_shift-4*decoding*")
+  paths$graphs <- file.path(paths$code, "utilities", "graphs.yml")
+  # source data:
+  paths$sourcedata <- file.path(paths$output, "sourcedata")
+  source_path <- file.path(paths$sourcedata, "zoo-sourcedata-%s")
+  paths$behav_task <- sprintf(source_path, "behavior-task")
+  paths$decoding_rest <- sprintf(source_path, "decoding-rest")
+  return(paths)
+}
+
+load_data <- function(paths_input) {
+  if (all(tools::file_ext(paths_input) == "")) {
+    paths_input <- Sys.glob(paste0(paths_input, ".*"))
+  } else {
+    paths_input <- Sys.glob(paste0(paths_input, "*"))
+  }
+  rds_files <- filter_paths(paths = paths_input, pattern = ".rds")
+  tsv_files <- filter_paths(paths = paths_input, pattern = ".tsv")
+  csv_files <- filter_paths(paths = paths_input, pattern = ".csv")
+  if (length(rds_files) > 0) {
+    data <- data.table::rbindlist(lapply(rds_files, readRDS))
+    file_type = ".rds"
+  } else if (length(tsv_files) > 0) {
+    data <- data.table::rbindlist(lapply(Sys.glob(tsv_files), data.table::fread))
+    file_type = ".tsv"
+  } else if (length(csv_files) > 0) {
+    data <- data.table::rbindlist(lapply(Sys.glob(csv_files), data.table::fread))
+    file_type = ".csv"
+  }
+  message <- sprintf("successfully imported data from %s", file_type)
+  status_report(text = message)
+  return(data)
+}
+
+save_data <- function(df, path) {
+  if (is.data.frame(df) == FALSE) {
+    stop("input is not a data.frame!")
+  }
+  if (is.null(path)) {
+    stop("no path provided!")
+  }
+  data.table::fwrite(setDT(df), paste0(path, ".tsv"), row.names = FALSE, sep = "\t")
+  saveRDS(df, paste0(path, ".rds"))
+  message <- sprintf("successfully exported %s", path)
+  status_report(text = message)
+  return(df)
+}
+
+filter_paths <- function(paths, pattern) {
+  paths_filtered <- paths[unlist(lapply(paths, function(x) grepl(pattern, x)))]
+  return(paths_filtered)
+}
+
+status_report <- function(text) {
+  stopifnot(is.character(text))
+  system2("echo", args = c(text))
+}
+
+load_graphs <- function(paths) {
+  graphs <- yaml::read_yaml(file = paths$graphs) %>%
+    plyr::ldply(data.frame, .id = "node_previous") %>%
+    gather(key, value, -node_previous) %>%
+    separate(key, into = c("node", "var"), extra = "merge") %>%
+    spread(var, value) %>%
+    setDT(.) %>%
+    .[, node_previous := as.factor(as.character(node_previous))] %>%
+    .[, node := as.factor(as.character(node))] %>%
+    .[, dist_bi := as.numeric(dist_bi)] %>%
+    .[, dist_flat := as.numeric(dist_flat)] %>%
+    .[, dist_uni := as.numeric(dist_uni)] %>%
+    .[, prob_bi := as.numeric(prob_bi)] %>%
+    .[, prob_flat := as.numeric(prob_flat)] %>%
+    .[, prob_uni := as.numeric(prob_uni)]
+  return(graphs)
+}
